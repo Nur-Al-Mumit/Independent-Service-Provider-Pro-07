cb816d259c643c4ae3fbfbc9198437375d694d76
nuralmumit <nuralmumit@kali.nuralmumit>
2022-04-19 20:07:49 +0600

    added banner & froms

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,11 +1,62 @@
-import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState } from "react";
+import { Carousel } from "react-bootstrap";
+
+/*
+src="https://manofmany.com/wp-content/uploads/2019/12/Bodyweight-Exercises.jpg"
+src="https://www.muscleandfitness.com/wp-content/uploads/2019/04/triceps-pushup-lean-muscular.jpg?quality=86&strip=all"
+src="https://media.gq-magazine.co.uk/photos/5d139941bc4bf6f0227f07e9/master/pass/IdoPortal-GQ-17Mar15-pr_b.jpg"
+*/
 
 const Banner = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+  return (
+    <div className="w-50 mx-auto mt-5">
+      <Carousel activeIndex={index} onSelect={handleSelect}>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://manofmany.com/wp-content/uploads/2019/12/Bodyweight-Exercises.jpg"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://www.muscleandfitness.com/wp-content/uploads/2019/04/triceps-pushup-lean-muscular.jpg?quality=86&strip=all"
+            alt="Second slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://media.gq-magazine.co.uk/photos/5d139941bc4bf6f0227f07e9/master/pass/IdoPortal-GQ-17Mar15-pr_b.jpg"
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
 };
 
 export default Banner;
--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import { Button, Form } from "react-bootstrap";
 
 const Register = () => {
-    return (
-        <div>
-            <h1>This is ypur Register</h1>
-        </div>
-    );
+  return (
+    <Form className="mt-5 w-50 mx-auto">
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Name</Form.Label>
+        <Form.Control type="text" placeholder="Enter Your Name" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button className="w-25 mx-auto" variant="primary" type="submit">
+        Register
+      </Button>
+    </Form>
+  );
 };
 
 export default Register;
